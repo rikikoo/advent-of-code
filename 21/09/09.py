5775a9dc89fe95fb2e2eca1fbed2e80024daa621
Riki Kyttälä <rkyttala@student.hive.fi>
2021-12-18 23:02:57 +0200

    added day 9 part 2 solution

--- 21/09/09.py
+++ 21/09/09.py
@@ -28,6 +28,42 @@ def check_surroundings(floor, curr_pos, limits):
         return 0
 
 
+def find_lowest_positions(floor, overlay, limits):
+    x_lim = limits[1]
+    y_lim = limits[0]
+    # iterate over seafloor
+    y = 0
+    while y <= y_lim:
+        x = 0
+        while x <= x_lim:
+            # update overlay
+            overlay[y, x] = check_surroundings(floor, (y, x), (y_lim, x_lim))
+            x += 1
+        y += 1
+    return overlay
+
+
+def flood_fill(floor, overlay, start, limits, depth):
+    surr = get_surrounding_pos(start, limits)
+    for adjacent in surr:
+        if floor[adjacent] != 9 and overlay[adjacent] == 0:
+            depth += 1
+            overlay[adjacent] = 1
+            overlay, depth = flood_fill(floor, overlay, adjacent, limits, depth)
+    return overlay, depth
+
+
+def get_lowest(floor, overlay):
+    low_points = []
+    low_positions = []
+    for y, row in enumerate(overlay):
+        for x, lowest in enumerate(row):
+            if lowest:
+                low_points.append(floor[y][x])
+                low_positions.append((y, x))
+    return low_points, low_positions
+
+
 def main():
     if len(sys.argv) < 2:
         print(f"Usage: {sys.argv[0]} <input filename>")
@@ -42,38 +78,24 @@ def main():
         print(f"Error opening file: {filename}")
         quit()
 
-    # init overlay matrix that will store min values
+    # init overlay matrix that will store min value positions
     overlay = np.zeros(floor.shape, dtype="int")
     # get edge row and column of matrix
-    x_lim = len(floor[0]) - 1
-    y_lim = len(floor) - 1
-    # iterate over seafloor
-    y = 0
-    while y <= y_lim:
-        x = 0
-        while x <= x_lim:
-            # update overlay
-            overlay[y, x] = check_surroundings(floor, (y, x), (y_lim, x_lim))
-            x += 1
-        y += 1
+    limits = (len(floor) - 1, len(floor[0]) - 1)
 
-    # find all low points on the seafloor map
-    low_points = []
-    for y, row in enumerate(overlay):
-        for x, lowest in enumerate(row):
-            if lowest:
-                low_points.append(floor[y][x])
+    # find values and positions of single lowest points
+    overlay = find_lowest_positions(floor, overlay, limits)
+    low_points, low_positions = get_lowest(floor, overlay)
+    print(f"Sum of risk levels of lowest points: {sum(low_points) + len(low_points)}")
 
-    # for line in floor:
-    #     for n in line:
-    #         print(str(n), end='')
-    #     print('')
-    # print('')
-    # for line in overlay:
-    #     for n in line:
-    #         print(str(n), end='')
-    #     print('')
-    print(f"Sum of risk levels: {sum(low_points) + len(low_points)}")
+    # do flood fill starting from each lowest point position
+    basins = []
+    for pos in low_positions:
+        overlay, basin_size = flood_fill(floor, overlay, pos, limits, 1)
+        basins.append(basin_size)
+    # find three largest basins
+    result = np.sort(basins)[-3:]
+    print(result.prod())
 
 
 if __name__ == "__main__":
